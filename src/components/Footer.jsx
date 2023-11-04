d4ca7d95790962cfdf18c2ddfbd57042e1cefa80
Mat <mathurin.malandain+github@gmail.com>
2023-11-04 17:21:16 +0100

    style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import '../styles/footer.css'
+import logoWhite from '../assets/img/kasa-logo-white.svg'
+
 function Footer() {
     return (
-        <div>
-            <h2>Footer</h2>
+        <div className="footer">
+            <img src={logoWhite} width="120" alt="Kaza logo blanc"/>
+            <p className="footer-text">© 2020 Kaza. All rights reserved</p>
         </div>
     )
 }
