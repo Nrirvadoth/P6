3707ff1027e1a0327806aba2023fa89b264bfca7
Mat <mathurin.malandain+github@gmail.com>
2023-11-04 12:27:17 +0100

    init project (CRA/router/assets/datas)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../assets/img/kasa-logo.svg'
+import logoWhite from '../../assets/img/kasa-logo-white.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoWhite} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
