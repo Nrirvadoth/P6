286c71255cf8f6ea707f0cbcd2d72d5e4852d2a7
Mat <mathurin.malandain+github@gmail.com>
2023-11-06 16:06:53 +0100

    style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import logo from '../assets/img/kasa-logo.svg'
 import { Link } from 'react-router-dom'
+import '../styles/header.css'
 
 function Header() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/a-propos">A propos</Link>
-        <Link to="/location">Lieu</Link>
-      </nav>
+    <header>
       <img src={logo} alt="Kaza logo"></img>
-      <h2>Header</h2>
-    </div>
+      <nav>
+        <ul>
+          <li key="navAccueil" ><Link to="/">Accueil</Link></li>
+          <li key="navAbout" ><Link to="/a-propos">A propos</Link></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
