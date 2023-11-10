d636a8f61e88f34dd6970e948df7163e875bd74e
Mat <mathurin.malandain+github@gmail.com>
2023-11-10 16:15:51 +0100

    Rating fonctionnel

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,29 @@
+import { ReactComponent as Star } from '../assets/img/star.svg'
+
 function Rating({ score }) {
-  return score
+  let content = []
+  const starActive = `star star-active`
+  const starInactive = `star star-inactive`
+
+  for (let i = 0; i < 5; i++) {
+    if (i < score) {
+      content.push(true)
+    } else {
+      content.push(false)
+    }
+  }
+
+  return (
+    <div className="rating">
+      {content.map((isActive) =>
+        isActive === true ? (
+          <Star className={starActive} />
+        ) : (
+          <Star className={starInactive} />
+        ),
+      )}
+    </div>
+  )
 }
 
 export default Rating
