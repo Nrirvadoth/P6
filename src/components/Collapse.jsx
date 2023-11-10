3b4430e548d929ae4913cef2e326a5409e8c2e76
Mat <mathurin.malandain+github@gmail.com>
2023-11-10 14:47:12 +0100

    composant collapse fonctionnel

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,20 +1,36 @@
 import '../styles/collapse.css'
 import arrow from '../assets/img/arrow.svg'
+import { useState, useRef } from 'react';
 
 function Collapse({ title, content }) {
+  const [style, setStyle] = useState("closed")
+  const [isOpen, setIsOpen] = useState(false);
+  const collapseStyle = `collapse-content ${style}`
+  const arrowStyle = `arrow-${style}`
+
+  function change() {
+    if (isOpen === true) {
+      setIsOpen(false)
+      setStyle("closed")
+    } else {
+      setIsOpen(true)
+      setStyle("open")      
+    }
+  }
+
   return (
     <div className="collapse-container">
       <div className="collapse-head">
         <h3 className="collapse-title">{title}</h3>
-        <img src={arrow} alt="" width="32" height="32" />
+        <img className={arrowStyle} src={arrow} alt="" width="32" height="32" onClick={change}/>
       </div>
       {typeof content === 'string' && (
-        <div className="collapse-content">
+        <div className={collapseStyle}>
           <p>{content}</p>
         </div>
       )}
       {typeof content === 'object' && (
-        <div className="collapse-content">
+        <div className={collapseStyle}>
           <ul>
             {content.map((item, index) => (
               <li key={`${item}-${index}`}>{item}</li>
