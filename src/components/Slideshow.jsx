8b558939d64b5df214829aa51ba1ef4a7a5fb3e0
Mat <mathurin.malandain+github@gmail.com>
2023-11-10 12:48:22 +0100

    slideshow fonctionnel

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,8 +1,34 @@
-function Slideshow({ picturesList, lenght }) {
-  const { currentImageUrl, setImageUrl }
+import { useState } from 'react'
+import leftArrow from '../assets/img/left-arrow.svg'
+import rightArrow from '../assets/img/right-arrow.svg'
+
+function Slideshow({ picturesList, length }) {
+  const [currentImageIndex, setIndex] = useState(0)
+
+  function next() {
+    setIndex(currentImageIndex + 1)
+    if (currentImageIndex === length - 1) {
+      setIndex(0)
+    }
+  }
+
+  function prev() {
+    setIndex(currentImageIndex - 1)
+    if (currentImageIndex === 0) {
+      setIndex(length - 1)
+    }
+  }
+
   return (
-    <div className="accomodation-gallery-container">
-      <img className="accomodation-gallery-item" src={picturesList[0]} alt="" />
+    <div className="slideshow-container">
+      <img className="slideshow-item" src={picturesList[currentImageIndex]} alt="" />
+      {length > 1 && (
+        <div className='slideshow-nav'>
+          <img src={leftArrow} onClick={prev} alt=""/>
+          <p className="counter">{currentImageIndex} / {length}</p>
+          <img src={rightArrow} onClick={next} alt=""/>
+        </div>
+      )}
     </div>
   )
 }
